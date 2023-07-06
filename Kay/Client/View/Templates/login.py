239a79004c61a4f47463b972a8dc2b3dfb89a88c
Kay318 <longguo0318@gmail.com>
2023-07-06 23:23:27 +0900

    로그인창 UI 완성, 마우스를 버튼에 갖다댈 때, 버튼이 밝아지면 더 좋을 것 같음.

--- Kay/Client/View/Templates/login.py
+++ Kay/Client/View/Templates/login.py
@@ -1,8 +1,8 @@
 # main.py
-import os
 import sys
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QSizePolicy,\
+from pathlib import Path
+from PyQt5.QtCore import Qt, QFile
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, \
                             QLabel, QLineEdit, QPushButton, QDesktopWidget, QHBoxLayout
 from PyQt5.QtGui import QPixmap
 
@@ -25,8 +25,8 @@ class LoginWindow(QWidget):
         self.setWindowFlags(self.windowFlags() & ~Qt.WindowMaximizeButtonHint) # 최대화 버튼 제거
         
         # 메인창 위치
-        self.setGeometry(0, 0, 400, 600)
-        self.setFixedSize(400, 600)
+        self.setGeometry(0, 0, 400, 650)
+        self.setFixedSize(400, 650)
         self._moveToCenter()
 
         # 레이아웃 설정
@@ -36,7 +36,7 @@ class LoginWindow(QWidget):
         self.setLayout(layout)
 
         # 이미지 레이블
-        pixmap = QPixmap(r'D:\g_Project\2023_skillup_chatting\Kay\Client\view\static\background.png')
+        pixmap = QPixmap(f'{Path(__file__).parents[1]}/static/background.png')
         imageLabel = QLabel()
         imageLabel.setPixmap(pixmap)
         imageLabel.setScaledContents(True)  # 추가된 줄
@@ -51,40 +51,62 @@ class LoginWindow(QWidget):
         # 이메일, 비밀번호 입력 필드
         emailField = QLineEdit()
         emailField.setPlaceholderText('Email')
-        layout.addWidget(emailField)
+        emailField.setFixedSize(350, 50)
+        
+        # QHBoxLayout를 생성하고 가운데 정렬을 적용한 후 메인 레이아웃에 추가
+        emailLayout = QHBoxLayout()
+        emailLayout.addWidget(emailField, alignment=Qt.AlignCenter)
+        layout.addLayout(emailLayout)
 
         passwordField = QLineEdit()
         passwordField.setPlaceholderText('PASSWORD')
-        layout.addWidget(passwordField)
+        passwordField.setFixedSize(350, 50)
+        
+        # QHBoxLayout를 생성하고 가운데 정렬을 적용한 후 메인 레이아웃에 추가
+        passwordLayout = QHBoxLayout()
+        passwordLayout.addWidget(passwordField, alignment=Qt.AlignCenter)
+        layout.addLayout(passwordLayout)
         
         # 로그인, 회원가입 버튼
         loginButton = QPushButton('로그인')
         loginButton.setObjectName('loginButton')
         loginButton.setCursor(Qt.PointingHandCursor)  # 마우스 오버 시 커서 변경
-        layout.addWidget(loginButton)  # layout에 추가
+        loginButton.setFixedSize(350, 50)
+        
+        # QHBoxLayout를 생성하고 가운데 정렬을 적용한 후 메인 레이아웃에 추가
+        buttonLayout = QHBoxLayout()
+        buttonLayout.addWidget(loginButton, alignment=Qt.AlignCenter)
+        layout.addLayout(buttonLayout)
 
         signupButton = QPushButton('회원가입')
         signupButton.setObjectName('signupButton')
         signupButton.setCursor(Qt.PointingHandCursor)  # 마우스 오버 시 커서 변경
-        layout.addWidget(signupButton)  # layout에 추가
+        signupButton.setFixedSize(350, 50)
+        
+        # QHBoxLayout를 생성하고 가운데 정렬을 적용한 후 메인 레이아웃에 추가
+        buttonLayout = QHBoxLayout()
+        buttonLayout.addWidget(signupButton, alignment=Qt.AlignCenter)
+        layout.addLayout(buttonLayout)
         
         # ID/PW 찾기, 관리자 문의 레이블
         bottomLayout = QHBoxLayout()  # 새로운 레이아웃 생성
         bottomLayout.setContentsMargins(20, 0, 20, 0)  # 좌, 상, 우, 하 마진 설정
+        
+        layout.addSpacing(20)
 
         findIDPWButton = QPushButton('ID/PW 찾기')
         findIDPWButton.setObjectName('findIDPW')
         findIDPWButton.setCursor(Qt.PointingHandCursor)  # 마우스 오버 시 커서 변경
-        findIDPWButton.setStyleSheet("text-align: left; background: none; border: none;")  # 레이블처럼 보이게 설정
         bottomLayout.addWidget(findIDPWButton, alignment=Qt.AlignLeft)  # 새로운 레이아웃에 추가
 
         adminContactButton = QPushButton('관리자 문의')
         adminContactButton.setObjectName('adminContact')
         adminContactButton.setCursor(Qt.PointingHandCursor)  # 마우스 오버 시 커서 변경
-        adminContactButton.setStyleSheet("text-align: right; background: none; border: none;")  # 레이블처럼 보이게 설정
         bottomLayout.addWidget(adminContactButton, alignment=Qt.AlignRight)  # 새로운 레이아웃에 추가
 
         layout.setContentsMargins(0, 0, 0, 50)
+        
+        self._setStyle()
         layout.addLayout(bottomLayout)  # 메인 레이아웃에 추가
 
     def _moveToCenter(self):
@@ -101,18 +123,21 @@ class LoginWindow(QWidget):
         # 移动窗口
         self.move(x, y)
         
-
-def main():
+    def _setStyle(self):
+        qss_file = QFile(f'{Path(__file__).parents[1]}/static/login.qss')
+        qss_file.open(QFile.ReadOnly | QFile.Text)
+        style_sheet = qss_file.readAll()
+        self.setStyleSheet(str(style_sheet, encoding='utf-8'))
+        
+        
+if __name__ == '__main__':
     app = QApplication(sys.argv)
 
     # 스타일 시트 적용
-    with open(r'D:\g_Project\2023_skillup_chatting\Kay\Client\view\static\login.qss', 'r', encoding='utf-8') as f:
-        app.setStyleSheet(f.read())
+    # with open(f'{Path(__file__).parents[1]}/static/login.qss', 'r', encoding='utf-8') as f:
+    #     app.setStyleSheet(f.read())
 
     loginWindow = LoginWindow()
     loginWindow.show()
 
     sys.exit(app.exec_())
-
-if __name__ == '__main__':
-    main()
