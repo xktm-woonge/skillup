a7b5374490485d8170a0e1723965de8f02c3fb6c
내비평가스크립트운영실/정용국 <9350816@ict-companion.com>
2023-08-03 14:54:55 +0900

    서버에서 전송하는 양식 통일

--- Kay/Server/controller/register_controller.js
+++ Kay/Server/controller/register_controller.js
@@ -1,39 +1,37 @@
 // ./controller/register_controller.js
 
 const logger = require('../utils/logger');
+const responseFormatter = require('../utils/responseFormatter');
 const { generateVerificationCode } = require('../utils/generateVerificationCode');
 const dbManager = require('../model/dbManager');
 const { sendEmail } = require('../model/emailService');
 
 
 exports.handleVerificationCodeRequest = function (message, session) {
+    logger.info(`message: ${JSON.stringify(message)}, session: ${JSON.stringify(session)}`)
     const email = message['info']['email'];
 
     dbManager.getUserByEmail(email, (error, user, fields) => {
         let response;
         if (error) {
-            response = {command: 'VERIFICATIONCODE', status: 'FAIL', message: '이메일 조회 중 에러가 발생했습니다.'};
-            logger.info(`response: ${JSON.stringify(response)}`);
-            session.socket.write(JSON.stringify(response));
+            response = responseFormatter.formatResponse('VERIFICATIONCODE', 'FAIL', '이메일 조회 중 에러가 발생했습니다.');
         } else if (user) {
-            response = {command: 'VERIFICATIONCODE', status: 'DUPLICATE', message: '이미 가입된 계정입니다.'};
-            logger.info(`response: ${JSON.stringify(response)}`);
-            session.socket.write(JSON.stringify(response));
+            response = responseFormatter.formatResponse('VERIFICATIONCODE', 'DUPLICATE', '이미 가입된 계정입니다.');
         } else {
             const verificationCode = generateVerificationCode();
 
             sendEmail(email, verificationCode, (error, info) => {
                 if (error) {
-                    response = {command: 'VERIFICATIONCODE', status: 'FAIL', message: '이메일 전송에 실패했습니다.'};
+                    response = responseFormatter.formatResponse('VERIFICATIONCODE', 'FAIL', '이메일 전송에 실패했습니다.');
                 } else {
                     session.verificationCode = verificationCode;
-                    response = {command: 'VERIFICATIONCODE', status: 'SUCCESS', message: '이메일 전송에 성공했습니다.'};
+                    response = responseFormatter.formatResponse('VERIFICATIONCODE', 'SUCCESS', '이메일 전송에 성공했습니다.');
                 }
 
-                logger.info(`response: ${JSON.stringify(response)}`);
-                session.socket.write(JSON.stringify(response));
             });    
         }
+        logger.info(`response: ${JSON.stringify(response)}`);
+        session.socket.write(JSON.stringify(response));
     });
 }
 
@@ -43,9 +41,9 @@ exports.handleVerify = function (message, session) {
     const received_code = message['info']['verification_code'];
 
     if (received_code === session.verificationCode) {
-        response = {command: 'VERIFY' , status: 'SUCCESS',  message: '인증에 성공했습니다.'};
+        response = responseFormatter.formatResponse('VERIFY', 'SUCCESS', '인증에 성공했습니다.');
     } else {
-        response = {command: 'VERIFY' , status: 'FAIL',  message: '인증에 실패했습니다. 다시 확인해 주세요.'};
+        response = responseFormatter.formatResponse('VERIFY', 'FAIL', '인증에 실패했습니다. 다시 확인해 주세요.');
     };
 
     logger.info(`response: ${JSON.stringify(response)}`);
@@ -62,10 +60,11 @@ exports.handleRegister = function (message, session) {
     dbManager.createUser(email, password, salt, (error, results, fields) => {
       let response;
       if (error) {
-        response = {command: 'REGISTER', status: 'FAIL', message: '회원가입에 실패했습니다.'};
+        response = responseFormatter.formatResponse('REGISTER', 'FAIL', '회원가입에 실패했습니다.');
       } else {
-        response = {command: 'REGISTER', status: 'SUCCESS', message: '축하합니다.\n회원가입에 성공했습니다.'};
+        response = responseFormatter.formatResponse('REGISTER', 'SUCCESS', '축하합니다.\n회원가입에 성공했습니다.');
       }
+      logger.info(`response: ${JSON.stringify(response)}`);
       session.socket.write(JSON.stringify(response));
     });
   }
