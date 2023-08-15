7a47f1a91bf4a6bb7fd21f67a754f37d113719d9
Kay318 <longguo0318@gmail.com>
2023-08-15 11:59:04 +0900

    sidebar를 별도의 모듈로 분리함

--- Kay/Client/View/Templates/chatting_main.py
+++ Kay/Client/View/Templates/chatting_main.py
@@ -11,7 +11,7 @@ import sys
 try:
     from utils import *
     from view.templates.chatting_sidebar import SidebarWidget
-    from view.templates.chatting_sidebar import ButtonLabelWidget
+    from view.templates.chatting_sidebar import Sidebar
     from view.templates.chatting_notifications import NotificationsListWidget
 except ImportError:
     import sys
@@ -19,7 +19,7 @@ except ImportError:
     sys.path.append(str(Path(__file__).parents[2]))
     from utils import *
     from view.templates.chatting_sidebar import SidebarWidget
-    from view.templates.chatting_sidebar import ButtonLabelWidget
+    from view.templates.chatting_sidebar import Sidebar
     from view.templates.chatting_notifications import NotificationsListWidget
 
 
@@ -32,140 +32,16 @@ class ChattingWindow(QWidget):
         self.right_width = 900
         self.height = 600
 
-        self.sidebar_icon_size = (25, 25)
-        self.sidebar_button_size = (50, 50)
-        
-        self.font = get_NotoSan_font()
-        self.font.setPointSize(12)
-
-        # Keep track of the current selected button
-        self.currentButton = None
-
         self.sidebar_expanded = False
         self.sidebar_labels = []
 
         self.initUI()
 
-        # Initialize labels as hidden
-        self.notification_label.hide()
-        self.friend_list_label.hide()
-        self.chat_window_label.hide()
-        self.profile_setting_label.hide()
-        self.setup_label.hide()
-
     def initUI(self):
 
-        # Side Bar
-        self.sidebar = SidebarWidget(self)
+        self.sidebar = Sidebar(self)
         self.sidebar.setGeometry(0, 0, self.sidebar_width, self.height)
-        # Sidebar 스타일
-        self.sidebar.setStyleSheet("""
-            background-color: white;
-        """)
 
-        side_layout = QVBoxLayout(self.sidebar)
-        side_layout.setContentsMargins(0, 0, 0, 0)  # Side layout border gap removal
-        side_layout.setSpacing(0)  # Gap removal between widgets inside side layout
-
-        notification_icon_path = f'{Path(__file__).parents[1]}/static/icon/-notifations-none_90255.svg'
-        notification_icon = QIcon(notification_icon_path)
-        self.notification_button = QPushButton()
-        self.notification_button.setIcon(notification_icon)
-        self.notification_button.setIconSize(QSize(*self.sidebar_icon_size))
-        self.notification_button.setFixedSize(*self.sidebar_button_size)
-        self.notification_button.setProperty('icon_path', notification_icon_path)
-
-        friend_list_icon_path = f'{Path(__file__).parents[1]}/static/icon/account_multiple_outline_icon_140049.svg'
-        friend_list_icon = QIcon(friend_list_icon_path)
-        self.friend_list_button = QPushButton()
-        self.friend_list_button.setIcon(friend_list_icon)
-        self.friend_list_button.setIconSize(QSize(*self.sidebar_icon_size))
-        self.friend_list_button.setFixedSize(*self.sidebar_button_size)
-        self.friend_list_button.setProperty('icon_path', friend_list_icon_path)
-
-        chat_window_icon_path = f'{Path(__file__).parents[1]}/static/icon/message_text_outline_icon_139369.svg'
-        chat_window_icon = QIcon(chat_window_icon_path)
-        self.chat_window_button = QPushButton()
-        self.chat_window_button.setIcon(chat_window_icon)
-        self.chat_window_button.setIconSize(QSize(*self.sidebar_icon_size))
-        self.chat_window_button.setFixedSize(*self.sidebar_button_size)
-        self.chat_window_button.setProperty('icon_path', chat_window_icon_path)
-
-        self.profile_setting_button = QPushButton()
-        self.profile_setting_button.setFixedSize(50, 50)
-        
-        # 온라인 상태를 표시하기 위한 QLabel 추가
-        self.status_label = QLabel()
-        self.status_label.setFixedSize(10, 10)
-        self.status_label.move(5, 5)  # 상태 원의 위치를 조정합니다
-        self.status_label.setParent(self.profile_setting_button)
-
-        setup_path = f'{Path(__file__).parents[1]}/static/icon/cog_outline_icon_139752.svg'
-        setup_icon = QIcon(setup_path)
-        self.setup_button = QPushButton()
-        self.setup_button.setIcon(setup_icon)
-        self.setup_button.setIconSize(QSize(*self.sidebar_icon_size))
-        self.setup_button.setFixedSize(*self.sidebar_button_size)
-        self.setup_button.setProperty('icon_path', setup_path)
-
-        self.notification_button.setObjectName('notification_button')
-        self.friend_list_button.setObjectName('friend_list_button')
-        self.chat_window_button.setObjectName('chat_window_button')
-        self.profile_setting_button.setObjectName('profile_setting_button')
-        self.setup_button.setObjectName('setup_button')
-
-        self.notification_label = QLabel('  알림', self)
-        self.notification_label.setFont(self.font)
-        self.notification_label.setObjectName('notification_label')
-        self.sidebar_labels.append(self.notification_label)
-
-        self.friend_list_label = QLabel('  친구 목록', self)
-        self.friend_list_label.setFont(self.font)
-        self.friend_list_label.setObjectName('friend_list_label')
-        self.sidebar_labels.append(self.friend_list_label)
-
-        self.chat_window_label = QLabel('  대화 내용', self)
-        self.chat_window_label.setFont(self.font)
-        self.chat_window_label.setObjectName('chat_window_label')
-        self.sidebar_labels.append(self.chat_window_label)
-
-        self.profile_setting_label = QLabel('  내 정보', self)
-        self.profile_setting_label.setFont(self.font)
-        self.profile_setting_label.setObjectName('profile_setting_label')
-        self.sidebar_labels.append(self.profile_setting_label)
-
-        self.setup_label = QLabel('  설정', self)
-        self.setup_label.setFont(self.font)
-        self.setup_label.setObjectName('setup_label')
-        self.sidebar_labels.append(self.setup_label)
-
-        # Notification button with label
-        self.notification_button_label_widget = ButtonLabelWidget(
-            self.notification_button, self.notification_label)
-        side_layout.addWidget(self.notification_button_label_widget)
-
-        # Friend list button with label
-        self.friend_list_button_label_widget = ButtonLabelWidget(
-            self.friend_list_button, self.friend_list_label)
-        side_layout.addWidget(self.friend_list_button_label_widget)
-
-        # Chat window button with label
-        self.chat_window_button_label_widget = ButtonLabelWidget(
-            self.chat_window_button, self.chat_window_label)
-        side_layout.addWidget(self.chat_window_button_label_widget)
-
-        # Profile setting button
-        self.profile_setting_button_label_widget = ButtonLabelWidget(
-            self.profile_setting_button, self.profile_setting_label, self)
-        side_layout.addWidget(self.profile_setting_button_label_widget)
-
-        # Add widgets to the side layout
-        side_layout.addStretch(1)
-
-        self.setup_button_label_widget = ButtonLabelWidget(
-            self.setup_button, self.setup_label)
-        side_layout.addWidget(self.setup_button_label_widget)
-        
         # Middle Area
         self.middle_area_widget = QStackedWidget(self)
         self.middle_area_widget.setGeometry(50, 0, self.middle_width, self.height)
@@ -202,24 +78,6 @@ class ChattingWindow(QWidget):
         # Clear widgets in middle_area and right_area
         self.middle_area_widget.setCurrentIndex(-1)
         self.right_area_widget.setCurrentIndex(-1)
-        
-    def handleButtonClicked(self, button):
-        if button != self.profile_setting_button:
-            if self.currentButton is not None:
-                self.currentButton.setStyleSheet("")
-                
-                if self.currentButton != self.profile_setting_button:
-                    icon_path = self.currentButton.property('icon_path')
-                    icon = QIcon(icon_path)
-                    self.currentButton.setIcon(icon)
-
-            icon_path = button.property('icon_path')
-            clicked_icon = change_svg_color(icon_path, "#FFFFFF")  # 흰색으로 변경
-            button.setIcon(clicked_icon)
-        
-        button.setStyleSheet("background-color: rgb(79, 42, 184);")
-
-        self.currentButton = button
 
     def enterEvent(self, event):
         self.sidebar.raise_()  # Make sure sidebar is above other widgets
@@ -243,15 +101,8 @@ class ChattingWindow(QWidget):
 
         super().resizeEvent(event)  # 이벤트의 부모 처리를 호출합니다.
 
-    def expandSidebar(self):
-        for label in self.sidebar_labels:
-            label.show()
-
-    def collapseSidebar(self):
-        for label in self.sidebar_labels:
-            label.hide()
-
     def _setStyle(self):
+        # Main QSS (if you have separate styles for main)
         qss_file = QFile(f'{Path(__file__).parents[1]}/static/chatting_main.qss')
         qss_file.open(QFile.ReadOnly | QFile.Text)
         style_sheet = qss_file.readAll()
--- Kay/Client/View/Templates/chatting_sidebar.py
+++ Kay/Client/View/Templates/chatting_sidebar.py
@@ -1,8 +1,11 @@
 # ./view/templates/chatting_sidebar.py
 
-from PyQt5.QtWidgets import QWidget, QHBoxLayout
+from PyQt5.QtWidgets import QWidget, QHBoxLayout, QVBoxLayout, \
+                            QPushButton, QLabel, QApplication
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QIcon
+from PyQt5.Qt import QSize
+from pathlib import Path
 
 try:
     from utils import *
@@ -11,6 +14,180 @@ except ImportError:
     from pathlib import Path
     sys.path.append(str(Path(__file__).parents[2]))
     from utils import *
+    
+
+class Sidebar(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.sidebar_labels = []
+        self.sidebar_icon_size = (25, 25)
+        self.sidebar_button_size = (50, 50)
+        self.sidebar_width_expanded = 150
+        self.sidebar_width_collapsed = 50
+        self.height = 600
+        self.setFixedWidth(self.sidebar_width_collapsed)
+        self.setMinimumHeight(self.height)
+        
+        self.setObjectName("sidebar")
+
+        self.font = get_NotoSan_font()
+        self.font.setPointSize(12)
+        self.currentButton = None
+
+        # Side layout
+        side_layout = QVBoxLayout(self)
+        side_layout.setContentsMargins(0, 0, 0, 0)
+        side_layout.setSpacing(0)
+        
+        notification_icon_path = f'{Path(__file__).parents[1]}/static/icon/-notifations-none_90255.svg'
+        notification_icon = QIcon(notification_icon_path)
+        self.notification_button = QPushButton()
+        self.notification_button.setIcon(notification_icon)
+        self.notification_button.setIconSize(QSize(*self.sidebar_icon_size))
+        self.notification_button.setFixedSize(*self.sidebar_button_size)
+        self.notification_button.setProperty('icon_path', notification_icon_path)
+
+        friend_list_icon_path = f'{Path(__file__).parents[1]}/static/icon/account_multiple_outline_icon_140049.svg'
+        friend_list_icon = QIcon(friend_list_icon_path)
+        self.friend_list_button = QPushButton()
+        self.friend_list_button.setIcon(friend_list_icon)
+        self.friend_list_button.setIconSize(QSize(*self.sidebar_icon_size))
+        self.friend_list_button.setFixedSize(*self.sidebar_button_size)
+        self.friend_list_button.setProperty('icon_path', friend_list_icon_path)
+
+        chat_window_icon_path = f'{Path(__file__).parents[1]}/static/icon/message_text_outline_icon_139369.svg'
+        chat_window_icon = QIcon(chat_window_icon_path)
+        self.chat_window_button = QPushButton()
+        self.chat_window_button.setIcon(chat_window_icon)
+        self.chat_window_button.setIconSize(QSize(*self.sidebar_icon_size))
+        self.chat_window_button.setFixedSize(*self.sidebar_button_size)
+        self.chat_window_button.setProperty('icon_path', chat_window_icon_path)
+
+        self.profile_setting_button = QPushButton()
+        self.profile_setting_button.setFixedSize(50, 50)
+        
+        # 온라인 상태를 표시하기 위한 QLabel 추가
+        self.status_label = QLabel()
+        self.status_label.setFixedSize(10, 10)
+        self.status_label.move(5, 5)  # 상태 원의 위치를 조정합니다
+        self.status_label.setParent(self.profile_setting_button)
+
+        setup_path = f'{Path(__file__).parents[1]}/static/icon/cog_outline_icon_139752.svg'
+        setup_icon = QIcon(setup_path)
+        self.setup_button = QPushButton()
+        self.setup_button.setIcon(setup_icon)
+        self.setup_button.setIconSize(QSize(*self.sidebar_icon_size))
+        self.setup_button.setFixedSize(*self.sidebar_button_size)
+        self.setup_button.setProperty('icon_path', setup_path)
+
+        self.notification_button.setObjectName('notification_button')
+        self.friend_list_button.setObjectName('friend_list_button')
+        self.chat_window_button.setObjectName('chat_window_button')
+        self.profile_setting_button.setObjectName('profile_setting_button')
+        self.setup_button.setObjectName('setup_button')
+
+        self.notification_label = QLabel('  알림', self)
+        self.notification_label.setFont(self.font)
+        self.notification_label.setObjectName('notification_label')
+        self.sidebar_labels.append(self.notification_label)
+
+        self.friend_list_label = QLabel('  친구 목록', self)
+        self.friend_list_label.setFont(self.font)
+        self.friend_list_label.setObjectName('friend_list_label')
+        self.sidebar_labels.append(self.friend_list_label)
+
+        self.chat_window_label = QLabel('  대화 내용', self)
+        self.chat_window_label.setFont(self.font)
+        self.chat_window_label.setObjectName('chat_window_label')
+        self.sidebar_labels.append(self.chat_window_label)
+
+        self.profile_setting_label = QLabel('  내 정보', self)
+        self.profile_setting_label.setFont(self.font)
+        self.profile_setting_label.setObjectName('profile_setting_label')
+        self.sidebar_labels.append(self.profile_setting_label)
+
+        self.setup_label = QLabel('  설정', self)
+        self.setup_label.setFont(self.font)
+        self.setup_label.setObjectName('setup_label')
+        self.sidebar_labels.append(self.setup_label)
+        
+        # Notification button with label
+        self.notification_button_label_widget = ButtonLabelWidget(
+            self.notification_button, self.notification_label)
+        side_layout.addWidget(self.notification_button_label_widget)
+
+        # Friend list button with label
+        self.friend_list_button_label_widget = ButtonLabelWidget(
+            self.friend_list_button, self.friend_list_label)
+        side_layout.addWidget(self.friend_list_button_label_widget)
+
+        # Chat window button with label
+        self.chat_window_button_label_widget = ButtonLabelWidget(
+            self.chat_window_button, self.chat_window_label)
+        side_layout.addWidget(self.chat_window_button_label_widget)
+
+        # Profile setting button
+        self.profile_setting_button_label_widget = ButtonLabelWidget(
+            self.profile_setting_button, self.profile_setting_label, self)
+        side_layout.addWidget(self.profile_setting_button_label_widget)
+
+        # Add widgets to the side layout
+        side_layout.addStretch(1)
+
+        self.setup_button_label_widget = ButtonLabelWidget(
+            self.setup_button, self.setup_label)
+        side_layout.addWidget(self.setup_button_label_widget)
+        
+        self.setLayout(side_layout)
+        self._setStyle()
+        
+        self.notification_label.hide()
+        self.friend_list_label.hide()
+        self.chat_window_label.hide()
+        self.profile_setting_label.hide()
+        self.setup_label.hide()
+            
+    def _setStyle(self):
+        with open(f'{Path(__file__).parents[1]}/static/chatting_sidebar.qss', 'r', encoding='utf-8') as file:
+            qss = file.read()
+            self.setStyleSheet(qss)
+        
+    def enterEvent(self, event):
+        self.setFixedWidth(self.sidebar_width_expanded)
+        self.expandSidebar()
+        QWidget.enterEvent(self, event)
+
+    def leaveEvent(self, event):
+        self.setFixedWidth(self.sidebar_width_collapsed)
+        self.collapseSidebar()
+        QWidget.leaveEvent(self, event)
+
+    def expandSidebar(self):
+        for label in self.sidebar_labels:
+            label.show()
+
+    def collapseSidebar(self):
+        for label in self.sidebar_labels:
+            label.hide()
+
+    def handleButtonClicked(self, button):
+        if button != self.profile_setting_button:
+            if self.currentButton is not None:
+                self.currentButton.setStyleSheet("")
+                
+                if self.currentButton != self.profile_setting_button:
+                    icon_path = self.currentButton.property('icon_path')
+                    icon = QIcon(icon_path)
+                    self.currentButton.setIcon(icon)
+
+            icon_path = button.property('icon_path')
+            clicked_icon = change_svg_color(icon_path, "#FFFFFF")  # 흰색으로 변경
+            button.setIcon(clicked_icon)
+        
+        button.setStyleSheet("background-color: rgb(79, 42, 184);")
+
+        self.currentButton = button
 
 
 class SidebarWidget(QWidget):
@@ -46,9 +223,6 @@ class ButtonLabelWidget(QWidget):
         self.label = label
         self.original_icon = QIcon(self.button.property('icon_path'))  # Save the original icon
 
-        # 이렇게 연결
-        self.button.clicked.connect(self.handleButtonClicked)
-
         layout = QHBoxLayout(self)
         layout.setContentsMargins(0, 0, 0, 0)  # Remove layout border gap
         layout.setSpacing(0)                   # Remove gap between widgets inside the layout
@@ -56,19 +230,21 @@ class ButtonLabelWidget(QWidget):
         layout.addWidget(label)
 
         self.setLayout(layout)
+        
+        self.button.clicked.connect(self.handleButtonClicked)
 
     def handleButtonClicked(self):
-        self.parent().parent().handleButtonClicked(self.button)
+        self.parent().handleButtonClicked(self.button)
 
     def enterEvent(self, event):
-        if self.parent().parent().currentButton:
-            self.parent().parent().currentButton.setStyleSheet("") 
-            if self.parent().parent().currentButton != self.parent().parent().profile_setting_button:
-                currentButton_iconPath = self.parent().parent().currentButton.property('icon_path')
+        if self.parent().currentButton:
+            self.parent().currentButton.setStyleSheet("") 
+            if self.parent().currentButton != self.parent().profile_setting_button:
+                currentButton_iconPath = self.parent().currentButton.property('icon_path')
                 black_icon = change_svg_color(currentButton_iconPath, "#000000")
-                self.parent().parent().currentButton.setIcon(black_icon)
+                self.parent().currentButton.setIcon(black_icon)
             
-        if self.button != self.parent().parent().profile_setting_button:
+        if self.button != self.parent().profile_setting_button:
             icon_path = self.button.property('icon_path')
             white_icon = change_svg_color(icon_path, "#FFFFFF")  # 흰색으로 변경
             self.button.setIcon(white_icon)
@@ -78,20 +254,27 @@ class ButtonLabelWidget(QWidget):
         QWidget.enterEvent(self, event)
 
     def leaveEvent(self, event):
-        if (self.button != self.parent().parent().profile_setting_button and
-            self.button != self.parent().parent().currentButton):
+        if (self.button != self.parent().profile_setting_button and
+            self.button != self.parent().currentButton):
             self.button.setIcon(self.original_icon)  # Restore the original icon
         
-        if (self.parent().parent().currentButton and
-            self.parent().parent().currentButton != self.parent().parent().profile_setting_button):
-            currentButton_iconPath = self.parent().parent().currentButton.property('icon_path')
+        if (self.parent().currentButton and
+            self.parent().currentButton != self.parent().profile_setting_button):
+            currentButton_iconPath = self.parent().currentButton.property('icon_path')
             white_icon = change_svg_color(currentButton_iconPath, "#FFFFFF")
-            self.parent().parent().currentButton.setIcon(white_icon)
+            self.parent().currentButton.setIcon(white_icon)
         
-        if self.parent().parent().currentButton:
-            self.parent().parent().currentButton.setStyleSheet("background-color: rgb(79, 42, 184);")
+        if self.parent().currentButton:
+            self.parent().currentButton.setStyleSheet("background-color: rgb(79, 42, 184);")
             
         self.setStyleSheet("")
         self.label.setStyleSheet("")
         self.setCursor(Qt.ArrowCursor)  # 일반 화살표 커서로 변경
         QWidget.leaveEvent(self, event)
+        
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    sidebar = Sidebar()
+    sidebar.show()
+    sys.exit(app.exec_())
