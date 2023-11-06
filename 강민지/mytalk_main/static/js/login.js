f3836c2153ac55c1e65b08afa698f81dc6ca9e1d
blistergoby <121704070+blistergoby@users.noreply.github.com>
2023-11-06 09:31:39 +0900

    commit_front_SCSS

---
+++ 강민지/mytalk_main/static/js/login.js
@@ -0,0 +1,53 @@
+function userLogin() {
+  var loginUserData = new FormData(
+    document.getElementById("send_login_user_info")
+  );
+  console.log(loginUserData.get("email") + loginUserData.get("password"));
+
+  // if (loginUserData.get('email') === ''){
+  //     alert('이메일을 입력해주세요.')
+  // } else if(loginUserData.get('password') === ''){
+  //     alert('비밀 번호를 입력해주세요.')
+  // } if{
+  fetch("/login_api/", {
+    method: "POST",
+    body: loginUserData,
+  })
+    .then(function (response) {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Error:" + response.status);
+      }
+    })
+    .then(function (data) {
+      if (data.message === "Success") {
+        window.location.href = "/main";
+      } else if (data.message === "Error" && data.error === "login_fail") {
+        alert("이메일 또는 비밀번호가 맞지 않습니다.");
+      }
+    });
+  // }
+}
+
+document
+  .getElementById("send_login_user_info")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
+    userLogin();
+  });
+
+
+window.addEventListener("DOMContentLoaded", function (e) {
+  const VISION_CHECK = document.getElementById("vision");
+  const PASSWORD_INPUT = document.querySelector('input[name="password"]');
+  const VISION_ICONS = document.querySelectorAll(".vision__icon");
+
+  VISION_ICONS.forEach((el) => {
+    el.addEventListener("click", function () {
+      VISION_CHECK.checked
+        ? (PASSWORD_INPUT.type = "password")
+        : (PASSWORD_INPUT.type = "text");
+    });
+  });
+});
